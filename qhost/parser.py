54845b3f640fb0738db551ba50b3adb1ad73de5a
Rob Lyon <nosignsoflifehere@gmail.com>
2014-01-21 21:50:08 -0800

    cleaning code

--- qhost/parser.py
+++ qhost/parser.py
@@ -1,6 +1,3 @@
-import xml.dom.minidom
-from qhost import Node
-
 # Copyright 2014, Rob Lyon <nosignsoflifehere@gmail.com>
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
@@ -14,6 +11,8 @@ from qhost import Node
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
+import xml.dom.minidom
+from qhost import Node
 
 
 class Parser:
